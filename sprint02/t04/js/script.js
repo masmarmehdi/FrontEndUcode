a802618f0d87adc2d45740ea261b4e4391d82526
masmarmehdi <masmar507@gmail.com>
2021-03-08 23:40:40 +0200

    t04

--- sprint02/t04/js/script.js
+++ sprint02/t04/js/script.js
@@ -1,8 +1,20 @@
 var beginRange = prompt("Enter the begining of the range: ");
-var endRange = prompt("Enter the end of the range: ")
+var endRange = prompt("Enter the end of the range: ");
 function checkDivision(beginRange, endRange){
     for(var i = beginRange ; i <= endRange; i++){
-        if(i % 2 == 0){
+        if(i % 2 == 0 && i % 3 == 0 && i % 10 == 0){
+            console.log(`${i} is even, a multiple of 3 , a multiple of 10`);
+        }
+        else if(i % 2 == 0 && i % 3 == 0){
+            console.log(`${i} is even and a multiple of 3`);
+        }
+        else if(i % 3 == 0 && i % 10 == 0){
+            console.log(`${i} is a multiple of 3 and a multiple of 10`);
+        }
+        else if(i % 2 == 0 && i % 10 == 0){
+            console.log(`${i} is even and a multiple of 10`)
+        }
+        else if(i % 2 == 0){
             console.log(`${i} is even`);
         }
         else if(i % 3 == 0){
@@ -13,3 +25,4 @@ function checkDivision(beginRange, endRange){
         }
     }
 }
+checkDivision(beginRange, endRange);
