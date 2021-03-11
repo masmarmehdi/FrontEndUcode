f6f5925be96a44ec91010ae484308410c1383fdd
Mehdi Masmar <mmasmar@c1r10p11.khpi.ucode-connect.study>
2021-03-11 19:39:12 +0200

    t00

--- sprint03/t00/js/script.js
+++ sprint03/t00/js/script.js
@@ -1,24 +1,31 @@
-/*let houseBlueprint={
-    address:,
-    date:,
-    description:,
-    owner,
-    size
-}*/
-function HouseBuilder(address, date, description, owner, size){
-    this.address = address;
-    this.date = date;
-    this.description = description;
+function HouseBuilder(adress, description, owner, size, roomCount){
+    this.adress = adress;
+    this.description =description;
     this.owner = owner;
     this.size = size;
+    this.roomCount = roomCount;
+    this._averageBuildSpeed = 0.5;
+    this.getDaysToBuild = function(){
+        return this.size / this._averageBuildSpeed;
+    }
 }
-
-const house = new HouseBuilder(
-    '88 Crescent Avenue',
-    'Spacious town house with wood flooring, 2-car garage, and a back patio.',
-    'J.Smith',
-    110);
-
-console.log(house.address);
-console.log(house.description);
-console.log(house.size);
+HouseBuilder.prototype = {
+    date:{ 
+        toDateString: function() {
+        var today = new Date();
+        var dd = today.getDay();
+        var currentday = String(today.getDate()).padStart(2, '0');
+        let day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+        var mm = today.getMonth(); //January is 0!
+        let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+        var yyyy = today.getFullYear();
+        return (day[dd - 1] + ' ' + month[mm]+ ' '+ currentday + ' ' + yyyy);
+        }
+    },
+}
+let house = new HouseBuilder('88 Crescent Avenue',
+'Spacious town house with wood flooring, 2-car garage, and a back patio.',
+'J. Smith',110,5);
+console.log(house.date.toDateString());
+console.log(house._averageBuildSpeed);
+console.log(house.getDaysToBuild());
