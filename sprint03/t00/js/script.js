85f5fda3fb26d32571dbddb1ee2f42f37f309e67
Mehdi Masmar <mmasmar@c1r11p11.khpi.ucode-connect.study>
2021-03-12 16:19:25 +0200

    t00

--- sprint03/t00/js/script.js
+++ sprint03/t00/js/script.js
@@ -9,19 +9,8 @@ function HouseBuilder(adress, description, owner, size, roomCount){
         return this.size / this._averageBuildSpeed;
     }
 }
-HouseBuilder.prototype = {
-    date:{ 
-        toDateString: function() {
-        var today = new Date();
-        var dd = today.getDay();
-        var currentday = String(today.getDate()).padStart(2, '0');
-        let day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-        var mm = today.getMonth(); //January is 0!
-        let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-        var yyyy = today.getFullYear();
-        return (day[dd - 1] + ' ' + month[mm]+ ' '+ currentday + ' ' + yyyy);
-        }
-    },
+houseBluePrint = HouseBuilder.prototype = {
+    date : new Date()
 }
 let house = new HouseBuilder('88 Crescent Avenue',
 'Spacious town house with wood flooring, 2-car garage, and a back patio.',
