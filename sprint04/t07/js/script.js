615bf4be26f67036a305acb46e6d4c0a51aaf237
Mehdi Masmar <mmasmar@c1r10p11.khpi.ucode-connect.study>
2021-03-18 23:27:36 +0200

    sprint04

---
+++ sprint04/t07/js/script.js
@@ -0,0 +1,38 @@
+const api = {
+    key: "004d057f9841da6c0d6c67de4be7d6cd",
+    base: "https://api.openweathermap.org/data/2.5/",
+    image: "http://openweathermap.org/img/wn/"
+}
+let body = document.getElementById('body');
+let city = document.getElementById('city');
+let forecastDate = document.getElementsByClassName('Date');
+let forecastImage = document.getElementsByClassName('Image');
+let forecastTemp = document.getElementsByClassName('Temp');
+function getCityInfo(query) {
+    fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => { return weather.json(); }).then(showInfo);
+}
+function showInfo(weather) {    
+    if (weather.cod === "404") {
+        console.error('Invalid city name. Rejected');
+        return;
+    }
+    city.innerText = `${weather.city.name}, ${weather.city.country}`;
+    console.log(weather)
+    showForecastDays(weather);
+}
+function showForecastDays(weather) {
+    let forecast = [0, 8, 16, 24, 32, 39];
+    for(let i = 0; i < forecast.length; i++) {
+        let now = new Date(weather.list[forecast[i]].dt_txt);
+        forecastDate[i].innerHTML = now.getDate() + "." + now.getMonth() + "." + now.getFullYear();
+        if(Math.round(weather.list[forecast[i]].main.temp) > 0){
+            forecastTemp[i].innerHTML = `+${Math.round(weather.list[forecast[i]].main.temp)}°C`;
+        }else{
+            forecastTemp[i].innerHTML = `${Math.round(weather.list[forecast[i]].main.temp)}°C`;
+        }
+        forecastImage[i].src = `${api.image}${weather.list[forecast[i]].weather[0].icon}@4x.png`;
+    }
+}
+var myCity = prompt("Enter your city:", "Kharkiv");
+getCityInfo(myCity);
