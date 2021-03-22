78d65a6d55ad7e16a248bb8ba0c7593c5914e00e
Mehdi Masmar <mmasmar@c1r10p11.khpi.ucode-connect.study>
2021-03-22 21:07:13 +0200

    race00

--- race00/js/script.js
+++ race00/js/script.js
@@ -12,6 +12,12 @@ function insert(number, output) {
     result.value += number;
 
 }
+var length = document.querySelector(".lenth");
+length.style.display = "none";
+var weight = document.querySelector(".weight");
+weight.style.display = "none";
+var square = document.querySelector(".square");
+square.style.display = "none";
 // decimal function
 function decimal(output) {
     let result = document.getElementsByClassName(output)[0];
@@ -29,7 +35,7 @@ function sqrt() {
         history.value = "";
     }
     else{
-        result.value = 'Are you dumb?';
+        result.value = 'Math Error';
     }
 }
 // operators function
@@ -57,7 +63,7 @@ function factorial() {
 }
 
 function percent() {
-    result.value = Number(result.value) / 100;
+    result.value = Number(result) / 100;
     history.value = "";
 }
 
@@ -80,6 +86,9 @@ function resultVal() {
         if (x == "+" || x == "*" || x == "-" || x == "%" || x == "÷" || x == "^" || x == "/") {
             example.splice(example.length - 2, 1);
         }
+        // if ( == 'Infinity') {
+        //     result.value = 'Math error';
+        // }
         if (example.indexOf('^') == -1) {
             result.value = String(eval(example.join('')));
         } else {
@@ -114,4 +123,107 @@ function replace(listStr, char, fun) {
         listStr.splice(i - 1, 1)
     }
     return listStr;
+}
+
+
+
+
+function lengthConv() {
+    if (length.style.display === "none") {
+        length.style.display = "block";
+    } else {
+        length.style.display = "none";
+    }
+  }
+const lengthConverter = (id, value) => {
+    let metr = document.querySelector("#inputMeters");
+    let cm = document.querySelector("#inputcm");
+    let km = document.querySelector("#inputKilometers");
+    let miles = document.querySelector("#inputMiles");
+    if(id == "inputMeters"){
+        cm.value = value*100;
+        km.value = value/1000;
+        miles.value = value*1609.34;
+    }
+    else if(id == "inputcm"){
+        metr.value = value/100;
+        km.value = value/100000;
+        miles.value = value/160934;
+    }
+    else if(id == "inputKilometers"){
+        metr.value = value*1000;
+        cm.value = value*100000;
+        miles.value = value/1.60934;
+    }
+    else if(id == "inputMiles"){
+        metr.value = value*1609.34;
+        cm.value = value*160934;
+        km.value = value*1.60934;
+    }
+}
+
+function weightConv() {
+    if (weight.style.display === "none") {
+        weight.style.display = "block";
+    } else {
+        weight.style.display = "none";
+    }
+  }
+const weightConverter = (id, value) => {
+    let kilogram = document.querySelector("#inputKilogram");
+    let pound = document.querySelector("#inputPound");
+    let gram = document.querySelector("#inputGram");
+    let tonne = document.querySelector("#inputTonne");
+
+    if(id == "inputKilogram"){
+        pound.value = value*2.20462;
+        gram.value = value*1000; 
+        tonne.value = value/1000;
+    }
+    else if(id == "inputPound"){
+        kilogram.value = value/2.20462;
+        gram.value = value*453.592; 
+        tonne.value = value/2204.62;
+    }
+    else if(id == "inputGram"){
+        kilogram.value = value/1000;
+        pound.value = value/453.592;
+        tonne.value = value/1e+6;
+    }
+    else if(id == "inputTonne"){
+        kilogram.value = value*1000;
+        pound.value = value*2204.62;
+        gram.value = value*1e+6; 
+    }
+}
+function squareConv() {
+    var square = document.querySelector(".square");
+    if (square.style.display === "none") {
+        square.style.display = "block";
+    } else {
+        square.style.display = "none";
+    }
+}
+const squareConverter = (id, value) => {
+    let millimeter = document.querySelector("#inputMillimeter");
+    let centimeter = document.querySelector("#inputCentimeter");
+    let meter = document.querySelector("#inputMeter");
+    let kilometer = document.querySelector("#inputKilometer");
+    if (id == "inputMillimeter"){
+        centimeter.value = value/100;
+        meter.value = value/1000000;
+        kilometer.value = value/1000000000000; 
+    } else if (id == "inputCentimeter") {
+        millimeter.value = value*100;
+        meter.value = value*0.0001;
+        kilometer.value = value/10000000000; 
+    } else if (id == "inputMeter") {
+        millimeter.value = value*1000000;
+        centimeter.value = value*10000;
+        kilometer.value = value*0.000001; 
+    } else if (id == "inputKilometer") {
+        millimeter.value = value*1000000000000;
+        centimeter.value = value*10000000000;
+        meter.value = value*1000000;
+    }
 }
