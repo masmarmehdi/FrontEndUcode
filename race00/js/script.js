09181d0e257996130a85856701b12089733116d7
masmarmehdi <masmar507@gmail.com>
2021-03-22 14:22:10 +0200

    race00

--- race00/js/script.js
+++ race00/js/script.js
@@ -1,69 +1,116 @@
-function getHistory(){
-    return document.getElementById("history").innerText;
+
+let history = document.getElementsByClassName("history")[0];
+let result = document.getElementsByClassName("result")[0];
+
+function insert(number, output) {
+    let result = document.getElementsByClassName(output)[0];
+    console.log(number);
+    console.log(result.value);
+    if (result.value == "") {
+        result.value = "";
+    }
+    result.value += number;
+
 }
 
-function historyValue(num){
-    return document.getElementById("history").innerHTML = num;
+function decimal(output) {
+    let result = document.getElementsByClassName(output)[0];
+    let example = result.value.split(' ');
+    console.log(example);
+    if (result.value != "" && result.value.indexOf(".") == -1) {
+        result.value += ".";
+    }
 }
 
-function getOutPut(){
-    return document.getElementById("result").innerHTML;
-}
-
-function printOutput(num){
-    if(num == ""){
-        document.getElementById("result").innerHTML = num;
+function sqrt() {
+    let result = document.getElementsByClassName("result")[0];
+    if (result.value >= 0) {
+        result.value = Math.sqrt(result.value).toFixed(5);
+        history.value = "";
     }
     else{
-        document.getElementById("result").innerHTML = num;
+        result.value = 'Are you dumb?';
     }
 }
 
-let operator = document.getElementsByClassName('operator');
-for(let i = 0; i < operator.length; i++){
-    operator[i].addEventListener('click', function(){
-        if(this.id == 'C'){
-            historyValue("");
-            printOutput("");
-        }
-        else if(this.id == 'backspace'){
-            let output = getOutPut().toString();
-            if(output){
-                output = output.substr(0,output.length-1);
-                printOutput(output);
-            }
-        }
-        else{
-            let output = getOutPut();
-            let history = getHistory();
-            if(output =="" && history != ""){
-                if(isNaN(history[history.length -1])){
-                    history = history.substr(0,history.length-1);
-                }
-            }
-            if(output != "" || history != ""){
-                history = history + output;
-                if(this.id == "equal"){
-                    let result = eval(history);
-                    printOutput(result);
-                    historyValue("");
-                }
-                else{
-                    history = history + this.id;
-                    historyValue(history);
-                    printOutput("");
-                }
-            }
-        }
-    })
+function operator(value) {
+    let x = history.value.slice(-1);
+    if(result.value == 0){
+        result.value == "";
+    }
+    else if (x !== "+" || x !== "*" || x !== "÷" || x !== "-" || x !== "%" || x !== "^") {
+        history.value += result.value;
+        
+        history.value = history.value + ' ' + value + ' ';
+        result.value = "";
+        
+    }
 }
-var number  = document.getElementsByClassName("number");
-for (let i  = 0; i < number.length; i++){
-    number[i].addEventListener('click', function(){
-        var output = getOutPut();
-        if(output != NaN){
-            output = output + this.id;
-            printOutput(output);
+function factorial() {
+    let result = document.getElementsByClassName("result")[0];
+    let factorial = 1;
+    for (let i = result.value; i > 0; i--) {
+        factorial *= i;
+    }
+    result.value = factorial;
+}
+
+function percent() {
+    result.value = Number(result.value) / 100;
+    history.value = "";
+}
+
+function plusMinus() {
+    history.value = "";
+    result.value = (-1) * Number(result.value);
+}
+
+function clearResult(result, history) {
+    document.getElementsByClassName(result)[0].value = "";
+    document.getElementsByClassName(history)[0].value = "";
+}
+
+function resultVal() {
+    if (history.value !== "") {
+        if (result.value !== "")
+            history.value += result.value;
+        let example = history.value.split(' ');
+        let x = example[example.length - 1] === "" ? example[example.length - 2] : example[example.length - 1];
+        if (x == "+" || x == "*" || x == "-" || x == "%" || x == "÷" || x == "^" || x == "/") {
+            example.splice(example.length - 2, 1);
         }
-    });
+        if (example.indexOf('^') == -1) {
+            result.value = String(eval(example.join('')));
+        } else {
+            result.value = eval(replace(example, "^", (prev, next) => {
+                return Math.pow(prev, next);
+            }).join(''));
+        }
+        history.value = "";
+    }
+}
+
+function saveValue() {
+    localStorage.setItem("KEY_VALUE", result.value)
+}
+
+function displaySavedValue(num) {
+    result.value = Number(localStorage.getItem("KEY_VALUE")) + num
+}
+
+function displaySavedValueSumResult(num) {
+    result.value = Number(localStorage.getItem("KEY_VALUE")) + result.value * num
+}
+
+
+
+function replace(listStr, char, fun) {
+    while (true) {
+        const i = listStr.indexOf(char)
+        if (i === -1) break;
+        listStr[i] = fun(listStr[i - 1], listStr[i + 1])
+        listStr.splice(i + 1, 1)
+        listStr.splice(i - 1, 1)
+    }
+    return listStr;
 }
